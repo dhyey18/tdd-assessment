4bf4d5de75fa0a1ac88735b9779ab8123f25cddc
dhyey18 <pateldhyey64@gmail.com>
2024-09-26 17:12:13 +0530

    success for test 2 "Single Number Returns That Number"

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,7 +1,9 @@
 
-// Implementation for Test 1
+// Implementation for Test 2
 function add(numbers) {
   if (!numbers) return 0;
+  return parseInt(numbers);
 }
+
 
 module.exports = { add };
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,7 +1,12 @@
 const { add } = require('./StringCalculator');
 
-// Test 1
+// Test 1: Empty String Returns 0
 test('should return 0 for an empty string', () => {
   expect(add("")).toBe(0);
+});
+
+// Test 2: Single Number Returns That Number
+test('should return the number for a single number', () => {
+  expect(add("1")).toBe(1);
 });
 
